48d17425fa76cb811603e193f42c47616b5b85f3
WalmerAlmeida <43154142+WalmerAlmeida@users.noreply.github.com>
2020-02-10 00:17:40 -0300

    Delete DadosLojaseContas.java

--- Projeto OO/src/com/company/DadosLojaseContas.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.util.ArrayList;
-
-public class DadosLojaseContas {
-    public static ArrayList<ContaLoja> Lojas = new ArrayList<ContaLoja>();
-    public static ArrayList<ContaLogin> Contas = new ArrayList<ContaLogin>();
-}
